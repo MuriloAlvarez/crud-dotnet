056dfa2c44fe1040d936e028124797c84efa7a11
MuriloAlvarez <murilo12alvarez@gmail.com>
2026-04-15 13:41:49 -0300

    Implement AppDbContext and Contact repository with configurations for contact entity

---
+++ crud-net/Infrastructure/Persistence/Configurations/ContactConfiguration.cs
@@ -0,0 +1,42 @@
+using crud_net.Features.Contacts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace crud_net.Infrastructure.Persistence;
+
+public sealed class ContactConfiguration : IEntityTypeConfiguration<Contact>
+{
+    public void Configure(EntityTypeBuilder<Contact> builder)
+    {
+        builder.ToTable("Contacts");
+
+        builder.HasKey(contact => contact.Id);
+
+        builder.Property(contact => contact.Name)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(contact => contact.DateOfBirth)
+            .IsRequired()
+            .HasColumnType("date");
+
+        builder.Property(contact => contact.Gender)
+            .IsRequired();
+
+        builder.Property(contact => contact.IsActive)
+            .IsRequired();
+
+        builder.Property(contact => contact.IsDeleted)
+            .IsRequired();
+
+        builder.Property(contact => contact.CreatedAtUtc)
+            .IsRequired();
+
+        builder.Property(contact => contact.UpdatedAtUtc);
+        builder.Property(contact => contact.DeactivatedAtUtc);
+        builder.Property(contact => contact.DeletedAtUtc);
+
+        builder.HasIndex(contact => contact.IsActive);
+        builder.HasIndex(contact => contact.IsDeleted);
+    }
+}
